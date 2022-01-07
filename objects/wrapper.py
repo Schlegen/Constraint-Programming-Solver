48c64d7f9fdf9d8469d89dd379b5d7c0ee1a6998
Maxime Brisinger <maximebrisinger@gmail.com>
2022-01-07 11:28:56 +0100

    implement a wrapper for constraints

---
+++ objects/wrapper.py
@@ -0,0 +1,42 @@
+from objects.constraint import Constraint
+
+
+def alldiff_constraint(variables, domains):
+    constraints = list()
+    for i in range(len(variables)):
+        for j in range(i + 1, len(variables)):
+            tuples = list()
+            for val_i in domains[variables[i]]:
+                for val_j in domains[variables[j]]:
+                    if val_i != val_j:
+                        tuples.append((val_i, val_j))
+            constraint = Constraint(variables[i], variables[j], tuples)
+            constraints.append(constraint)
+    return constraints
+
+
+# acj + bci =! j - i ET bcj + aci =! j - i
+def queens_columns_constraint(variables, domains, a=1, b=-1):
+    constraints = list()
+    for i in range(len(variables)):
+        for j in range(i + 1, len(variables)):
+            tuples = list()
+            for val_i in domains[variables[i]]:
+                for val_j in domains[variables[j]]:
+                    if a * val_j + b * val_i != j - i and b * val_j + a * val_i != j - i:
+                        tuples.append((val_i, val_j))
+            constraint = Constraint(variables[i], variables[j], tuples)
+            constraints.append(constraint)
+    return constraints
+
+
+# ax + by = c
+def weighted_sum_constraint(variables, domains, a, b, c):
+    constraints = None
+    return constraints
+
+
+
+
+
+
