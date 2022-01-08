c144efb5a3c48e483b5ba038af0473167c52d2e7
Nicolas Schlegel <Nicolas Schlegel>
2022-01-08 16:24:21 +0100

    affichage de la cartographie

--- cartography.py
+++ cartography.py
@@ -3,25 +3,63 @@ from objects.domain import Domain
 from objects.wrapper import cartography_constraints
 from csp import CSP
 from utils.parser import parse_carto
+import matplotlib.pyplot as plt
+import networkx as nx
+from random import randint
+import matplotlib.pyplot as plt
+
 
 
 class Cartography(CSP):
     def __init__(self, nb_colors, file_name):
 
-        nb_nodes, edges = parse_carto(file_name)
+        self.nb_colors = nb_colors
+
+        self.nb_nodes, self.edges = parse_carto(file_name) 
+        # print(self.edges)
 
         # Variables
-        variables = [Variable("x" + str(i)) for i in range(1, nb_nodes + 1)]
+        variables = [Variable("x" + str(i)) for i in range(1, self.nb_nodes + 1)]
 
         # Domains
         domains = Domain(variables)
         domains.fill_all_domains_by_range(lb=1, ub=nb_colors)
 
         # Constraints
-        constraints = cartography_constraints(variables, domains.dict, edges)
+        constraints = cartography_constraints(variables, domains.dict, self.edges)
+        
         # print([[v.name for v in c.variables] for c in constraints])
 
         super().__init__(variables=variables, domains=domains, constraints=constraints)
+
+    def show_solution(self):
+        if solution is None:
+            return 0
+
+        G = nx.Graph()
+
+        # colors = []
+        # for _ in range(self.nb_colors):
+        #     colors.append('#%06X' % randint(0, 0xFFFFFF))
+        
+        # for key, color_code in self.final_solution.items():
+        #     print(key)
+
+        #nodes = [(key, {"color" : "red"}) for key, color_code in self.final_solution.items()]
+        #print(nodes)
+        G.add_nodes_from(self.final_solution.keys())#nodes)
+
+        # print(G.nodes)
+        # print(self.edges)
+        # print([(list(G.nodes)[e[0]-1], list(G.nodes)[e[1]-1]) for e in self.edges])
+        G.add_edges_from([(list(G.nodes)[e[0]-1], list(G.nodes)[e[1]-1]) for e in self.edges])
+
+        nx.draw(G, with_labels=True, cmap=plt.cm.tab20, node_color=list(self.final_solution.values()))
+        plt.show()          
+        return 0
+
+
+
 
 
 colors = 5
@@ -33,3 +71,4 @@ print(f"Solving Cartography Problem with n = {colors} colors and instance {file.
 solution = cartography.main(instantiation=dict())
 print(f"\nThere is a solution : {solution}")
 print(cartography.final_solution)
+cartography.show_solution()
